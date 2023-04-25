e352bd4918aa45d3a092b8bef2b12ef909b3f996
Mateusz Furga <matfurga@gmail.com>
2023-04-25 18:37:48 +0200

    Add build.sh

--- lab5zd/dynamic/invoke-client/build.sh
+++ lab5zd/dynamic/invoke-client/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-g++ -I. -DICE_CPP11_MAPPING -c client.cc Invoke.cpp
+mkdir stub
+slice2cpp --output-dir stub Invoke.ice
+g++ -Istub -DICE_CPP11_MAPPING -c client.cc stub/Invoke.cpp
 g++ -o client Invoke.o client.o -lIce++11
 
